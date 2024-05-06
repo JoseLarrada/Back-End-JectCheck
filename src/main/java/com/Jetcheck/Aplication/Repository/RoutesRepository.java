dc271ba9b86b27cec6e9d77ac943704df72abb21
LAURABRITOARRIETA <lcbrito@unicesar.edu.co>
2024-05-05 19:08:04 -0500

    Cambios en repositorio de rutas y servicios de rutas

--- src/main/java/com/Jetcheck/Aplication/Repository/RoutesRepository.java
+++ src/main/java/com/Jetcheck/Aplication/Repository/RoutesRepository.java
@@ -1,11 +1,12 @@
 package com.Jetcheck.Aplication.Repository;
 
 import com.Jetcheck.Aplication.Entity.Rutas;
-import com.Jetcheck.Aplication.Entity.Usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface RoutesRepository extends JpaRepository<Rutas,String> {
+    void deleteByNombre(String nombre);
+    Optional<Rutas> findByNombre(String nombre);
+    boolean existsByNombre(String nombre);
 }
